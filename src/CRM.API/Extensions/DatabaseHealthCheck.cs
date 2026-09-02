5c5c338e69cce969c9a14b3aa258831c4e0e7e32
vannhan-1602 <vovannhan160204@gmail.com>
2026-09-02 08:39:23 +0700

    them healthcheck db cho backend

---
+++ src/CRM.API/Extensions/DatabaseHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using CRM.Infrastructure.Persistence.Contexts;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CRM.API.Extensions;
+
+
+public class DatabaseHealthCheck : IHealthCheck
+{
+    private readonly CrmDbContext _context;
+
+    public DatabaseHealthCheck(CrmDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var canConnect = await _context.Database.CanConnectAsync(cancellationToken);
+            return canConnect
+                ? HealthCheckResult.Healthy("Database connection is healthy.")
+                : HealthCheckResult.Unhealthy("Cannot connect to database.");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy("Database health check threw an exception.", ex);
+        }
+    }
+}
